be0a3cf52a0a1e1e5e20d226dcf5c4cd08cb7ba3
catmorgan <morgcat11@gmail.com>
2017-08-17 18:17:36 -0700

    able to destruct walls closes #9

---
+++ UnityRogueLike/Assets/Scripts/Wall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite DamagedSprite;
+    public int HitPoints = 4;
+
+    private SpriteRenderer _spriteRenderer;
+
+    public void DamageWall(int loss)
+    {
+        _spriteRenderer.sprite = DamagedSprite;
+        HitPoints -= loss;
+
+        if (HitPoints <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void Awake ()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
